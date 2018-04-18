080203d137bb4b5b2c45b5f5062b3449fa8b9d2f
benfurber <ben@furber-consultancy.com>
2018-04-18 14:37:19 +0100

    Test and working method to dock bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :dock
+
+  def initialize
+    @dock = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @dock << bike
+    "Bike docked"
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,8 +4,12 @@ describe DockingStation do
   it { should respond_to :release_bike }
 
   it 'releases bike' do
-  bike = subject.release_bike
-  expect(bike).to be_working
-end
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
+  it 'docks bike' do
+    bike = subject.dock_bike(Bike.new)
+    expect(bike).to eq 'Bike docked'
+  end
 end
