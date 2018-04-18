d92de00d5711928506808aeb1fde937d3cf83cd6
benfurber <ben@furber-consultancy.com>
2018-04-18 16:07:31 +0100

    Half way through raising an error test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,14 +5,17 @@ class DockingStation
 
   def initialize
     @dock = []
+    @bike_free = false
   end
 
   def release_bike
+    raise NoBikeError if @bike_free
+    @bike_free = true
     Bike.new
   end
 
   def dock_bike(bike)
     @dock << bike
-    "Bike docked"
+    'Bike docked'
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,23 +3,27 @@ require 'docking_station'
 describe DockingStation do
   it { should respond_to :release_bike }
 
-  it 'releases bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-
-  it 'docks bike' do
-    bike = subject.dock_bike(Bike.new)
-    expect(bike).to eq 'Bike docked'
-  end
-
   it 'shows the empty dock' do
     expect(subject.dock).to eq []
   end
 
-  it 'shows the bike(s) docked' do
-    subject.dock_bike(Bike.new)
-    expect(subject.dock.count).to eq 1
+  let(:bike) { subject.release_bike }
+  before { bike }
+
+  it 'releases bike' do
+    expect(bike).to be_working
   end
 
+  it 'docks bike' do
+    expect(subject.dock_bike(bike)).to eq 'Bike docked'
+  end
+
+  it 'shows the bike(s) docked' do
+    subject.dock_bike(bike)
+    expect(subject.dock).to include bike
+  end
+
+  it 'should raise an error if no bikes are free' do
+    expect(subject.release_bike).to raise_error(NoBikeError)
+  end
 end
